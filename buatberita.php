3fb10cd7335cbc9dc2ede8f4bedcb1cbf31e066b
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2020-10-19 21:35:08 +0700

    fixing buatberita

--- buatberita.php
+++ buatberita.php
@@ -4,26 +4,41 @@ include 'koneksi.php';
 ?>
 
 <form method='POST'>
-<table>
-<tr><td><b>Judul Berita: </b></td> <td><input  type='text' name='judul' style='width:500px' /></td></tr>
-<tr><td><b>Isi berita: </b> </td><td><textarea name='isi' cols='100' rows='10' tabindex='4'></textarea></td><tr>
-<tr><td><b>URL: </b></td> <td><input  type='text' name='url' style='width:300px' /></td></tr>
-</table>
-          <input type="submit" value="Tambah!" name="BtnAdd" ></td>
-		  
+	<table>
+		<tr>
+			<td><b>Judul Berita: </b></td>
+			<td><input type='text' name='judul' style='width:500px' /></td>
+		</tr>
+		<tr>
+			<td><b>Isi berita: </b> </td>
+			<td><textarea name='isi' cols='100' rows='10' tabindex='4'></textarea></td>
+		<tr>
+		<tr>
+			<td><b>URL: </b></td>
+			<td><input type='text' name='url' style='width:300px' /></td>
+		</tr>
+	</table>
+	<input type="submit" value="Tambah!" name="BtnAdd"></td>
 </form>
 
 <?php
-if(isset($_POST['BtnAdd'])){ // jika tombol 'BtnAdd' di klik, lakukan proses:
-// ambil judul, isi berita, url
-$judul1 = $_POST['judul'];
-$isi1 = $_POST['isi'];
-$url1 = $_POST['url'];
-$id1 = str_replace(" ", "_", $judul1); // replace spasi dgn '_' utk dijadikan id_berita/
+if (isset($_POST['BtnAdd'])) { // jika tombol 'BtnAdd' di klik, lakukan proses:
+	$judul = $_POST['judul'];
+	$isi = $_POST['isi'];
+	$url = $_POST['url'];
 
-// masukkan ke database
-		$query = "INSERT INTO berita VALUES ('0','$id1','$judul1','$isi1','$url1')"; 
-		$insert_query  = mysqli_query($koneksi,$query);
+	// masukkan ke database
+	try {
+		$query = "INSERT INTO berita (`judul`, `isi`, `url`) VALUES ('$judul', '$isi', '$url')";
+		mysqli_query($koneksi, $query);
+
+		echo "Data Tersimpan";
+
+	} catch (\Throwable $th) {
+		echo $th->getMessage();
+
+	}
 }
-
 ?>
+
+<?php include 'footer.php' ?>
